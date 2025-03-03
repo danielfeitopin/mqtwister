7327f15971ce9a5ddabdd9a294710cef5f14438c
Daniel Feito Pin <danielfeitopin@protonmail.com>
2025-03-03 11:37:33 +0100

    Read NIC name from config file

--- mqtwister/__main__.py
+++ mqtwister/__main__.py
@@ -1,8 +1,9 @@
 from . import context
-from .config import TARGET_IP
+from .config import TARGET_IP, INTERFACE_NAME
 from .cli.banner import Banner
 from .utils.logging import logger
-from .utils.network import get_arp_table, get_mac_address
+from .utils.network import get_arp_table, get_interface_mac
+import sys
 
 # Display banner
 print(Banner.get_colorful_banner(Banner.DEFAULT_COLOR))
@@ -14,12 +15,19 @@ context['ARP_TABLE'] = get_arp_table()
 logger.debug(f"ARP table: {context['ARP_TABLE']}")
 
 # Get own MAC address
-context['OWN_MAC_ADDRESS'] = get_mac_address()
+context['OWN_MAC_ADDRESS'] = get_interface_mac(INTERFACE_NAME)
+if not context.get('OWN_MAC_ADDRESS'):
+    msg = f"MAC address not found for interface '{INTERFACE_NAME}'"
+    logger.error(msg)
+    print(msg)
+    sys.exit(1)
 print(f"Own MAC address: {context['OWN_MAC_ADDRESS']}")
 
 # Get target MAC address
 context['TARGET_MAC_ADDRESS'] = context['ARP_TABLE'].get(TARGET_IP)
-if context.get('TARGET_MAC_ADDRESS') is None:
-    print(f"Target's MAC not found!")
-    exit(1)
+if not context.get('TARGET_MAC_ADDRESS'):
+    msg = f"Target's MAC not found!"
+    logger.error(msg)
+    print(msg)
+    sys.exit(1)
 print(f"Target MAC address: {context['TARGET_MAC_ADDRESS']}")
--- mqtwister/config.py
+++ mqtwister/config.py
@@ -1,5 +1,10 @@
-TARGET_IP: str = ''
+from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
 
 # Debugging
-from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
 LOGGING_LEVEL: int = DEBUG
+
+# Host configuration
+INTERFACE_NAME: str = ''
+
+# Target configuration
+TARGET_IP: str = ''
