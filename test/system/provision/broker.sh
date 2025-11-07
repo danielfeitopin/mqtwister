115c398f602f6d0b7fb89f5bd7578e941df6e865
Daniel Feito Pin <danielfeitopin@protonmail.com>
2025-11-07 20:24:04 +0100

    test: Improve test environment provisioning
    Add Mosquittto logging.
    Make subscriber client read configuration file instead of declaring constant broker IP.

--- test/system/provision/broker.sh
+++ test/system/provision/broker.sh
@@ -19,7 +19,14 @@ install -o root -g root -m 600 -t /root /tmp/provision_files/.bash_profile
 # === Set up service ===
 
 # Copy the Mosquitto configuration file
-install -Dt /etc/mosquitto /tmp/provision_files/mosquitto.conf
+install -o mosquitto -g mosquitto -Dt /etc/mosquitto /tmp/provision_files/mosquitto.conf
+
+# Create log directory if it doesn't exist
+if [ -f /var/log/mosquitto.log ]; then
+    echo "Log file '/var/log/mosquitto.log' already exists"
+else
+    install -o mosquitto -g mosquitto -m 640 -D /dev/null /var/log/mosquitto.log
+fi
 
 # Enable the service to start on boot
 rc-update add mosquitto default
--- test/system/provision/files/subscriber/subscribe_colors.sh
+++ test/system/provision/files/subscriber/subscribe_colors.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-BROKER_IP="192.168.56.10"
+# Load configuration
+config_file="/vagrant/subscribe_colors.conf"
+if [ -f $config_file ]; then
+    source $config_file
+else
+    echo "Configuration file '$config_file' not found!"
+    exit 1
+fi
 
 while true; do
     mosquitto_sub -h "$BROKER_IP" -t "colors" | while read line; do
